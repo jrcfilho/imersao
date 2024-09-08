031af29f056d645cf34da33ef77769e33f9fc354
Jodilson Coelho <140405554+jrcfilho@users.noreply.github.com>
2024-09-07 22:10:35 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+function pesquisar() {
+    // Seleciona a seção onde os resultados da pesquisa serão exibidos
+    let section = document.querySelector(".resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+
+    console.log(campoPesquisa);
+    // Imprime a seção no console para fins de depuração (opcional)
+    console.log(section);
+    
+if (!campoPesquisa) {
+    section.innerHTML = "<p>Faça uma nova Busca!</p>"
+    return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre os dados da pesquisa
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        
+        // Constrói o HTML para cada item da pesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">
+                    ${dado.descricao}
+                </p>
+                <a href=${dado.link} target="_blank">Mais informações clique aqui!</a>
+            </div>
+        `;
+    } 
+    }
+    if (!resultados) {
+        section.innerHTML = "<p>Nada foi encontrado. Faça uma nova Busca, Obrigado!</p>"
+    return
+    }
+
+    // Atribui o HTML gerado à seção dos resultados
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+      titulo: "Demon Slayer",
+      descricao: "Demon Slayer, ou Kimetsu no Yaiba em japonês, é um anime que conquistou o coração de milhões de fãs ao redor do mundo. A história gira em torno de Tanjiro Kamado, um jovem carvoeiro que tem sua vida drasticamente alterada após um ataque demoníaco que massacra sua família.",
+      link: "https://pt.wikipedia.org/wiki/Kimetsu_no_Yaiba",
+      tags: "demonio demon espadachin hashira"
+    },
+    {
+      titulo: "My Hero Academia",
+      descricao: "My Hero Academia é um anime que se passa em um mundo onde a maioria das pessoas possuem superpoderes, chamados de Quirks. A história acompanha Izuku Midoriya, um garoto que nasceu sem poderes, mas que sonha em se tornar um herói.",
+      link: "https://pt.wikipedia.org/wiki/Boku_no_Hero_Academia",
+      tags: "menino heroi escola"
+    },
+    {
+      titulo: "Naruto",
+      descricao: "Naruto é um anime que conta a história de um jovem ninja chamado Naruto Uzumaki que busca reconhecimento e sonha em se tornar o Hokage, o líder da vila da folha.",
+      link: "https://pt.wikipedia.org/wiki/Naruto",
+       tags: "kiubi konoha vila"    }
+  ];
